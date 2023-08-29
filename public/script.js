2d0a66d6df41bc8d6ede1e10fb882464dd773c81
Michael Dorst <m@mdorst.net>
2023-08-29 13:56:50 -0700

    Reanimate current page link after mouse leaves links

--- public/script.js
+++ public/script.js
@@ -1,60 +1,110 @@
-const animationDelayMs = 60
+const ANIMATION_DELAY_MS = 60;
 
 const links = {
   "link-home": {
     text: "Home",
     element: null,
-    animating: false,
+    animationInterval: null,
     mouseOver: false,
+    currentPage: false,
   },
   "link-projects": {
     text: "Projects",
     element: null,
-    animating: false,
+    animationInterval: null,
     mouseOver: false,
+    currentPage: false,
   },
   "link-info": {
     text: "Info",
     element: null,
-    animating: false,
+    animationInterval: null,
     mouseOver: false,
+    currentPage: false,
   },
   "link-contact": {
     text: "Contact",
     element: null,
-    animating: false,
+    animationInterval: null,
     mouseOver: false,
+    currentPage: false,
   },
-}
+};
+let currentPageLink = null;
 
 function animateWriting(link) {
-  link.animating = true
-  let numCharsWritten = 0
-  link.element.text = "> _"
-  const interval = setInterval(() => {
-    if (!link.mouseOver) {
-      clearInterval(interval)
-      link.element.innerHTML = `&nbsp;&nbsp;${link.text}`
-      return
+  let numCharsWritten = 0;
+  link.element.text = "> _";
+
+  function animationFrame() {
+    const partialText = link.text.slice(0, numCharsWritten++);
+    link.element.text = `> ${partialText}_`;
+    if (partialText === link.text) {
+      clearInterval(link.animationInterval);
     }
-    const partialText = link.text.slice(0, numCharsWritten++)
-    link.element.text = `> ${partialText}_`
-  }, animationDelayMs)
+  }
+
+  animationFrame();
+  const interval = setInterval(animationFrame, ANIMATION_DELAY_MS);
+  return interval;
 }
 
-const linkElements = document.querySelectorAll('nav a')
+function mouseOverLink(link) {
+  clearAllAnimationIntervals();
+  currentPageLink.element.innerHTML = `&nbsp;&nbsp;${currentPageLink.text}`;
+  link.animationInterval = animateWriting(link);
+}
+
+function mouseLeaveLink(link) {
+  if (link === currentPageLink) {
+    return;
+  }
+  clearInterval(link.animationInterval);
+  link.animationInterval = null;
+  link.element.innerHTML = `&nbsp;&nbsp;${link.text}`;
+  setTimeout(reanimateCurrentPageLink, ANIMATION_DELAY_MS);
+}
+
+function reanimateCurrentPageLink() {
+  // Don't reanimate current page link if another link is hovered
+  for (const linkId in links) {
+    const link = links[linkId];
+    if (link.mouseOver) {
+      return;
+    }
+  }
+  if (currentPageLink.animationInterval === null) {
+    currentPageLink.animationInterval = animateWriting(currentPageLink);
+  }
+}
+
+function clearAllAnimationIntervals() {
+  for (const linkId in links) {
+    const link = links[linkId];
+    clearInterval(link.animationInterval);
+    link.animationInterval = null;
+  }
+}
+
+const linkElements = document.querySelectorAll("nav a");
 
 for (const linkElement of linkElements) {
-  links[linkElement.id].element = linkElement
+  const link = links[linkElement.id];
+  link.element = linkElement;
+  if (linkElement.classList.contains("current-page")) {
+    link.currentPage = true;
+    currentPageLink = link;
+  }
 }
 
 for (const linkId in links) {
-  const link = links[linkId]
-  link.element.addEventListener('mouseover', () => {
-    link.mouseOver = true
-    animateWriting(link)
-  })
-  link.element.addEventListener('mouseleave', () => {
-    link.mouseOver = false
-  })
+  const link = links[linkId];
+  link.element.addEventListener("mouseover", () => {
+    link.mouseOver = true;
+    mouseOverLink(link);
+  });
+  link.element.addEventListener("mouseleave", () => {
+    link.mouseOver = false;
+    mouseLeaveLink(link);
+  });
 }
