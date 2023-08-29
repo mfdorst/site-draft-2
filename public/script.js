a27c532653ad85bd8c8ff65713a2ea31d3c27011
Michael Dorst <m@mdorst.net>
2023-08-29 01:36:02 -0700

    Show link hovering with script

--- public/script.js
+++ public/script.js
@@ -1,7 +1,17 @@
+const idTextMap = {
+  "link-home": "Home",
+  "link-projects": "Projects",
+  "link-info": "Info",
+  "link-contact": "Contact",
+}
+
 const links = document.querySelectorAll('nav a');
 
 for (const link of links) {
-  link.addEventListener('mouseover', (e) => {
-    console.log(e)
+  link.addEventListener('mouseover', () => {
+    link.text = `> ${idTextMap[link.id]}_`
+  })
+  link.addEventListener('mouseleave', () => {
+    link.innerHTML = `&nbsp;&nbsp;${idTextMap[link.id]}`
   })
 }
