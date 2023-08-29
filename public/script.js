9106d20cfe6d72777146920e2c3bec98c83164db
Michael Dorst <m@mdorst.net>
2023-08-29 11:41:01 -0700

    Implement link animation

--- public/script.js
+++ public/script.js
@@ -1,17 +1,60 @@
-const idTextMap = {
-  "link-home": "Home",
-  "link-projects": "Projects",
-  "link-info": "Info",
-  "link-contact": "Contact",
+const animationDelayMs = 60
+
+const links = {
+  "link-home": {
+    text: "Home",
+    element: null,
+    animating: false,
+    mouseOver: false,
+  },
+  "link-projects": {
+    text: "Projects",
+    element: null,
+    animating: false,
+    mouseOver: false,
+  },
+  "link-info": {
+    text: "Info",
+    element: null,
+    animating: false,
+    mouseOver: false,
+  },
+  "link-contact": {
+    text: "Contact",
+    element: null,
+    animating: false,
+    mouseOver: false,
+  },
 }
 
-const links = document.querySelectorAll('nav a');
+function animateWriting(link) {
+  link.animating = true
+  let numCharsWritten = 0
+  link.element.text = "> _"
+  const interval = setInterval(() => {
+    if (!link.mouseOver) {
+      clearInterval(interval)
+      link.element.innerHTML = `&nbsp;&nbsp;${link.text}`
+      return
+    }
+    const partialText = link.text.slice(0, numCharsWritten++)
+    link.element.text = `> ${partialText}_`
+  }, animationDelayMs)
+}
 
-for (const link of links) {
-  link.addEventListener('mouseover', () => {
-    link.text = `> ${idTextMap[link.id]}_`
+const linkElements = document.querySelectorAll('nav a')
+
+for (const linkElement of linkElements) {
+  links[linkElement.id].element = linkElement
+}
+
+for (const linkId in links) {
+  const link = links[linkId]
+  link.element.addEventListener('mouseover', () => {
+    link.mouseOver = true
+    animateWriting(link)
   })
-  link.addEventListener('mouseleave', () => {
-    link.innerHTML = `&nbsp;&nbsp;${idTextMap[link.id]}`
+  link.element.addEventListener('mouseleave', () => {
+    link.mouseOver = false
   })
 }
